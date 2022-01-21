df00d7aca34159dfaa9e36aaefaa45497a049c05
DPG896 <82310008+DPG896@users.noreply.github.com>
2022-01-21 15:54:30 +0800

    Update Main.php

--- InstaHub/src/DPGD896/Hub/Main.php
+++ InstaHub/src/DPGD896/Hub/Main.php
@@ -1,42 +1,31 @@
 <?php
 
-namespace DPGD896\Hub;
+namespace DENPlayz414502\HubPM4;
 
 use pocketmine\Server;
-use pocketmine\Player;
+use pocketmine\player\Player;
 
-use pocketmine\utils\Config;
-use pocketmine\plugin\PluginBase;
+use pocketmine\PluginBase;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
-use pocketmine\event\Listener;
+class Main extends PluginBase {
 
-class Main extends PluginBase implements Listener {
-	
-	public function onEnable(){
-	$this->getServer()->getLogger()->info("Enabled Plugin");
-        //to create file config.yml
-        @mkdir($this->getDataFolder());
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-	}
-	
-	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
-	
-	      switch($cmd->getName()){
-                 case "hub":
-	               if($sender instanceof Player){
-                               //teleport player to default world and get spawn is safe to player
-		               $sender->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-                               $sender->addTitle($this->config->get("title-hub"), $this->config->get("sub-hub"),);;
-                               $sender->sendMessage($this->config->get("message-hub"));
-		               //$sender->addtitle("§a§lTeleported", "§7§lto Lobby!",);
-		               //$sender->sendMessage("§aTeleported!");
-                  }
-	     }
-	return true;
-	}
-	
-	
+    public function onEnable(): void {
+        $this->getLogger()->info("Plugin Enabled");
+    }
+
+    public function onDisable(): void {
+        $this->getLogger()->info("Plugin Disabled")
+    }
+
+    public function onCommand(CommandSender $sender , Command $cmd, String $label, Array $args) : bool {
+
+        if($cmd->getName() == "test"){
+            $sender->sendMessage("A test command");
+        }
+    return true;
+    }
+
 }
