121e243799e1c486edf5982262d688234bd87b4a
DPG896 <82310008+DPG896@users.noreply.github.com>
2022-02-28 13:40:25 +0800

    pm4 update (tangina poggit accept nyo to hahaha)

--- InstaHub/src/DPGD896/Hub/Main.php
+++ InstaHub/src/DPGD896/Hub/Main.php
@@ -5,36 +5,33 @@ namespace DPGD896\Hub;
 use pocketmine\Server;
 use pocketmine\Player;
 
-use pocketmine\utils\Config;
 use pocketmine\plugin\PluginBase;
 
 use pocketmine\command\Command;
 use pocketmine\command\CommandSender;
 
+use pocketmine\event\player\PlayerJoinEvent;
 use pocketmine\event\Listener;
 
-class Main extends PluginBase implements Listener {
-	
-	public function onEnable(){
-	$this->getServer()->getLogger()->info("Enabled Plugin");
-        //to create file config.yml
-        @mkdir($this->getDataFolder());
-        $this->config = new Config($this->getDataFolder() . "config.yml", Config::YAML);
-	}
-	
-	public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args) : bool {
-	
-	      switch($cmd->getName()){
+class main extends PluginBase implements Listener {
+    
+    public function onEnable() : void{
+        
+    }
+    
+    
+    public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args): bool {
+    
+          switch($cmd->getName()){
                  case "hub":
-	               if($sender instanceof Player){
-                               //teleport player to default world and get spawn is safe to player
-		               $sender->teleport($this->getServer()->getDefaultLevel()->getSafeSpawn());
-                               $sender->addTitle($this->config->get("title-hub"), $this->config->get("sub-hub"),);;
-                               $sender->sendMessage($this->config->get("message-hub"));
+                   if ($sender->hasPermission("insta.hub")){
+                                   $world = $this->getServer()->getWorldManager()->getWorldByName("world");
+                       $sender->teleport($world->getSafeSpawn());
+                       $sender->sendtitle("Teleported", "to Lobby!",);
+                       $sender->sendMessage("Teleported!");
                   }
-	     }
-	return true;
-	}
-	
-	
+         }
+      return true;
+    }
+    
 }
