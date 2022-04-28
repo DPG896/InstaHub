71906291e01acb813590a2f66a34da118bf4803f
DPG896 <82310008+DPG896@users.noreply.github.com>
2022-04-29 05:22:38 +0800

    Update Main.php

--- InstaHub/src/DPGD896/Hub/Main.php
+++ InstaHub/src/DPGD896/Hub/Main.php
@@ -23,7 +23,7 @@ class Main extends PluginBase implements Listener {
     public function onCommand(CommandSender $sender, Command $cmd, String $label, Array $args): bool {
         switch($cmd->getName()){
             case "hub":
-                if($sender->hasPermission("insta.hub")){
+                if($sender->hasPermission("instahub.command")){
                     if($sender instanceof Player) {
                         $world = $this->getServer()->getWorldManager()->getWorldByName($this->getConfig()->get("world"));
                         $sender->teleport($world->getSafeSpawn());
